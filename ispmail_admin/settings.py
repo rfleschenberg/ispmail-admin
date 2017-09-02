b1fd15dedd05dc7bb07d338576e45d3c05588456
René Fleschenberg <rene@fleschenberg.net>
2017-09-02 16:34:29 +0200

    Login view, basic template code

--- ispmail_admin/settings.py
+++ ispmail_admin/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'ispmail_auth',
 ]
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'ispmail_admin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
